f4b36656d64af34ef5bb05976cfc022c8f62c1d0
Abdellatif Edlby <abdellatif_e@hotmail.com>
2025-03-13 12:45:47 +0200

    refactor: remove commented-out redirect from root to login

--- next.config.mjs
+++ next.config.mjs
@@ -15,11 +15,6 @@ const nextConfig = {
   },
   async redirects() {
     return [
-      // {
-      //   source: "/",
-      //   destination: "/login",
-      //   permanent: true,
-      // },
       {
         source: "/admin",
         destination: "/admin/dashboard",
