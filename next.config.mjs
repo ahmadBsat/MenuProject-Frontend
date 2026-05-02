e4fde692796576b7d4466be31bd4875c8309b892
ahmadBsat <64708488+ahmadBsat@users.noreply.github.com>
2026-05-02 22:22:19 +0300

    Add new hostname for object storage configuration

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "fmc-store.fsn1.your-objectstorage.com",
+      },
+      {
+        protocol: "https",
         hostname: "maps.googleapis.com",
       },
       {
